f24be10bc940d3f4417deec6e602d3cd46a30b46
Jecheon Ryu <rjc1704@gmail.com>
2026-02-10 18:44:57 +0900

    초기화

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Hiiiii Express! DB_HOST: ${process.env.DB_HOST} </h1>`);
+  res.send(`<h1>Hello Express! DB_HOST: ${process.env.DB_HOST} </h1>`);
 });
 
 app.listen(3000, () => {
